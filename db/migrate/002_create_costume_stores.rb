de9af87e543cfcd4943833f42081e6780b62eab0
Robinson Regalado <rregalado17@gmail.com>
2019-09-09 16:46:17 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,15 @@
 # Create your costume_stores migration here
 
 class CreateCostumesStores < ActiveRecord::Migration[5.2]
-  
+  class CreateCostumes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url 
+      t.integer :size 
+      t.datetime :timestamp
+    end
+  end
+end
 end
