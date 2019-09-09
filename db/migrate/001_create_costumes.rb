977fbb060f05e742f366eaadfc1471cd71fe8fc7
Robinson Regalado <rregalado17@gmail.com>
2019-09-09 16:33:48 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,7 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change 
     create_table :costumes do |t|
-      t.string :name
+      t.string :name,
       t.integer :price
     end
   end
