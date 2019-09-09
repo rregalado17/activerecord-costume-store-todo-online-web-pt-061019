e62d45da39a003b9cf0a6bda2bf946d358cb4c56
Robinson Regalado <rregalado17@gmail.com>
2019-09-09 16:38:28 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,6 +13,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
+      t.string :image_url 
+      
     end
   end
 end
